dd96088897c5e22b4dbe83a6aba2e52bcb8855ca
mirandago <mmgo@ucsd.edu>
2020-11-08 11:40:22 -0800

    fixed lint errors

--- code/newPage.js
+++ code/newPage.js
@@ -11,47 +11,53 @@
 //   console.log('Value currently is ' + result.counter);
 // });
 
-// show the current stored value
-function buttonClick(e) {
-  if (window.localStorage.getItem("runTest")) {
-    document.getElementById("counter").innerText = window.localStorage.getItem("testCounter");
+/**
+ * Shows the current stored value
+ */
+function buttonClick() {
+  if (window.localStorage.getItem('runTest')) {
+    const testCounter = window.localStorage.getItem('testCounter');
+    document.getElementById('counter').innerText = testCounter;
   } else {
     chrome.storage.sync.get(['counter'], function(result) {
-      document.getElementById("counter").innerText = result.counter;
+      document.getElementById('counter').innerText = result.counter;
       console.log('Value currently is ' + result.counter);
     });
   }
 }
 
-// set stored value to 0
-function setClick(e) {
-  var theValue = 0;
-  if (window.localStorage.getItem("runTest")) {
-    document.getElementById("counter").innerText = theValue;
+/**
+ * Sets the stored value to 0
+ */
+function setClick() {
+  const theValue = 0;
+  if (window.localStorage.getItem('runTest')) {
+    document.getElementById('counter').innerText = theValue;
   } else {
     chrome.storage.sync.set({'counter': theValue}, function() {
       console.log('Value is set to ' + theValue);
-      document.getElementById("counter").innerText = theValue;
+      document.getElementById('counter').innerText = theValue;
     });
   }
 }
 
-// increment storage value
-function incrementClick(e) {
-  if (window.localStorage.getItem("runTest")) {
-    var incValue = 1 + parseInt(window.localStorage.getItem("testCounter"));
-    document.getElementById("counter").innerText = incValue;
-    window.localStorage.setItem("testCounter", incValue);
+/**
+ * Increments the stored value
+ */
+function incrementClick() {
+  if (window.localStorage.getItem('runTest')) {
+    const incValue = 1 + parseInt(window.localStorage.getItem('testCounter'));
+    document.getElementById('counter').innerText = incValue;
+    window.localStorage.setItem('testCounter', incValue);
   } else {
     chrome.storage.sync.get(['counter'], function(result) {
       if (!result) {
-        document.getElementById("counter").innerText = result.counter;
-      }
-      else {
-        var incValue = result.counter + 1;
+        document.getElementById('counter').innerText = result.counter;
+      } else {
+        const incValue = result.counter + 1;
         chrome.storage.sync.set({'counter': incValue}, function() {
           console.log('Value is set to ' + incValue);
-          document.getElementById("counter").innerText = incValue;
+          document.getElementById('counter').innerText = incValue;
         });
       }
     });
@@ -59,8 +65,9 @@ function incrementClick(e) {
 }
 
 // Add click event listeners
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById("demo").addEventListener("click", buttonClick);
-  document.getElementById("set").addEventListener("click", setClick);
-  document.getElementById("increment").addEventListener("click", incrementClick);
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('demo').addEventListener('click', buttonClick);
+  document.getElementById('set').addEventListener('click', setClick);
+  document.getElementById('increment').
+      addEventListener('click', incrementClick);
 });
