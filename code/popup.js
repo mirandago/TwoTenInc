0aafa735026b10a6c6902267da7922f28ca75e73
Megan Chu <megan.chu2@gmail.com>
2020-11-08 16:57:14 -0800

    headless testing

---
+++ code/popup.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Execute when button is clicked
+ */
+function click() {
+  // check that event listener works by changing original popup
+  document.getElementById('hello').innerHTML = 'clicked';
+  document.body.style.backgroundColor='red';
+
+  // make a new tab and a new window
+  // chrome.tabs.create({url: "http://www.stackoverflow.com"});
+  chrome.windows.create({url: 'newPage.html', type: 'popup'});
+  // window.close();
+}
+
+// add click even listener to all 'h2' elements in popup
+document.addEventListener('DOMContentLoaded', function() {
+  const divs = document.querySelectorAll('h2');
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
