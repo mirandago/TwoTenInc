e675c19654fdd5339e995e2efb34e57d96fbf9b9
David Liu <davidliu@doyouknowthewei.attlocal.net>
2020-11-11 18:51:17 -0800

    timer with a bit of css

--- code/timer.js
+++ code/timer.js
@@ -1,27 +1,49 @@
-var DefaultTime = 1500;
+var DefaultTime = 5;
+var DefaultFocusTime = 5;
+var DefaultBreakTime = 3;
 var DefaultActive = false;
 var DefaultFocus = true;
+var DefaultFocusText = "Focus";
+var DefaultBreakText = "Break";
 
 class Timer {
     
-    constructor(time_left, active, focus, id) {
+    constructor(time_left, active, focus, id, stateId) {
         this.time_left = time_left;
         this.active = active;
         this.focus = focus;
         this.timerId = id;
+        this.stateId = stateId;
         document.getElementById(id).innerText = time_left;
-        console.log("hello");
+        document.getElementById(stateId).innerText = DefaultFocusText;
     }
     
+    switchState() {
+        if (this.focus) {
+            this.focus = false;
+            document.getElementById(this.stateId).innerText = DefaultBreakText;
+            document.getElementById(this.timerId).innerText = DefaultBreakTime;
+            this.time_left = DefaultBreakTime;
+        } else {
+            this.focus = true;
+            document.getElementById(this.stateId).innerText = DefaultFocusText;
+            document.getElementById(this.timerId).innerText = DefaultFocusTime;
+            this.time_left = DefaultFocusTime;
+        }
+        this.startTimer();
+    }
+
     updateTimerDisplay() {
         this.time_left--;
         document.getElementById(this.timerId).innerText = this.time_left;
+        if (this.time_left == 0) {
+            this.pauseTimer();
+            setTimeout(this.switchState.bind(this), 1000);
+        }
     }
 
     startTimer() {
         this.active = true;
-        var self = this;
-        var time_left = this.time_left;
         var id = this.timerId;
         console.log("what is id" + id);
         this.runningCall = setInterval(
@@ -49,9 +71,13 @@ class Timer {
     isFocus() {
         return this.focus;
     }
+
+    isActive() {
+        return this.active;
+    }
 }
 
-var timer = new Timer(DefaultTime, DefaultActive, DefaultFocus, "timer");
+var timer = new Timer(DefaultFocusTime, DefaultActive, DefaultFocus, "timer", "timer_state");
 
 document.getElementById("play_button").addEventListener("click", function(){
     timer.startTimer();
