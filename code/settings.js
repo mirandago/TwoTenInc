3398dfa9912489e4797edb5c02afec9b9a9b6749
Megan Chu <megan.chu2@gmail.com>
2020-11-12 19:12:45 -0800

    fix indentation

--- code/settings.js
+++ code/settings.js
@@ -17,17 +17,17 @@ document.addEventListener('DOMContentLoaded', function() {
   const _selector1 = document.querySelector('input[type=checkbox]#timer');
   _selector1.addEventListener('change', function (event) {
     if (_selector1.checked) {
-        document.body.style.backgroundColor='red';
+      document.body.style.backgroundColor='red';
     } else {
-        document.body.style.backgroundColor='green';
+      document.body.style.backgroundColor='green';
     }
   });
   const _selector2 = document.querySelector('input[type=checkbox]#break');
   _selector2.addEventListener('change', function (event) {
     if (_selector2.checked) {
-        document.body.style.backgroundColor='blue';
+      document.body.style.backgroundColor='blue';
     } else {
-        document.body.style.backgroundColor='orange';
+      document.body.style.backgroundColor='orange';
     }
   });
 });
