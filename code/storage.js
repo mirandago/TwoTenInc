f39fe46c16fa1cab97e58158a695274e7fc7bc27
Ruixian Song <ruixiansong@Ruixians-MacBook-Pro.local>
2020-11-30 11:17:24 -0800

    Add storage

---
+++ code/storage.js
@@ -0,0 +1,154 @@
+/** Add a new group
+* @param {String} group The color of the group
+* @return {Boolean} Whether the group exists
+*/
+export async function addGroup(group) {
+  let existed = await new Promise(function(resolve, reject){
+    chrome.storage.sync.get([group], function(result) {
+      if(typeof(result[group]) == 'undefined') {
+        let tasks = [];
+        chrome.storage.sync.set({[group]: tasks}, function() {
+          resolve(true);
+          console.log('Group ' + group + ' added');
+        });
+      } else {
+        resolve(false);
+        console.log('Group ' + group + ' exists');
+      }
+    });
+  });
+  console.log(existed);
+  return existed;
+}
+
+/** Get all groups
+* @return {Array} A list of all groups
+*/
+export async function getGroups() {
+  let groups = await new Promise(function(resolve, reject){
+    chrome.storage.sync.get(null, function(result) {
+      resolve(Object.keys(result));
+    });
+  });
+  console.log(groups);
+  return groups;
+}
+
+/** Add a new task to a specific group
+* @param {String} name The name of the task
+* @param {Number} session The number of sessions for this task
+* @param {String} group The group the task will be assigned to
+* @return {Boolean} Whether the task exists
+*/
+export async function addTask(name, session, group) {
+  let d = new Date();
+  let date = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
+  let task = {name: name, group: group, session: session, date: date, completed: false};
+
+  let existed = await new Promise(function(resolve, reject){
+    chrome.storage.sync.get([group], function(result) {
+      let tasks = result[group];
+      let e = false;
+      for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].name == name){
+          e = true;
+          break;
+        }
+      }
+      if(!e) {
+        tasks.push(task);
+        chrome.storage.sync.set({[group]: tasks}, function() {
+          console.log("Task added");
+        });
+        resolve(false);
+      } else {
+        console.log("Task exists");
+        resolve(true);
+      }
+    });
+  });
+  console.log(existed);
+  return existed;
+}
+
+/** Get all tasks
+* @return {Array} A list of all tasks
+*/
+export async function getAllTasks() {
+  let groups = await getGroups();
+  let allTasks = [];
+  for(let i = 0; i < groups.length; i++) {
+    let tasks = await getTasksByGroup(groups[i]);
+    allTasks.push.apply(allTasks, tasks);
+  }
+  console.log(allTasks);
+  return allTasks;
+}
+
+/** Get all tasks in a specific group
+* @param {String} group The name of the group
+* @return {Array} A list of tasks in the group
+*/
+async function getTasksByGroup(group) {
+  let tasks = await new Promise(function(resolve, reject){
+    chrome.storage.sync.get([group], function(result) {
+      resolve(result[group]);
+    });
+  });
+  return tasks;
+}
+
+/** Delete a task in a group
+* @param {String} name The name of the task
+* @param {String} group The name of the group
+*/
+export function deleteTask(name, group) {
+  chrome.storage.sync.get([group], function(result) {
+    let tasks = result[group];
+    for(let i = 0; i < tasks.length; i++) {
+      if(tasks[i].name == name) {
+        tasks.splice(i, 1);
+      }
+    }
+    chrome.storage.sync.set({[group]: tasks}, function() {
+      console.log("Task deleted");
+    });
+  });
+}
+
+/** Complete a task in a group
+* @param {String} name The name of the task
+* @param {String} group The name of the group
+*/
+export function completeTask(name, group) {
+  chrome.storage.sync.get([group], function(result) {
+    let tasks = result[group];
+    for(let i = 0; i < tasks.length; i++) {
+      if(tasks[i].name == name) {
+        tasks[i].completed = true;
+      }
+    }
+    chrome.storage.sync.set({[group]: tasks}, function() {
+      console.log("Task Completed");
+    });
+  });
+}
+
+/** Complete one session of a task in a group
+* @param {String} name The name of the task
+* @param {String} group The name of the group
+*/
+export function completeSession(name, group) {
+  chrome.storage.sync.get([group], function(result) {
+    let tasks = result[group];
+    for(let i = 0; i < tasks.length; i++) {
+      if(tasks[i].name == name) {
+        tasks[i].session--;
+      }
+    }
+    chrome.storage.sync.set({[group]: tasks}, function() {
+      console.log("Session completed");
+    });
+  });
+}
+
