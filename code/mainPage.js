a3105f7fae2dd52a0a442f46830872862060a593
divyankrahoria <divyankrahoria@gmail.com>
2020-12-08 13:29:37 -0800

    saving task attempt

--- code/mainPage.js
+++ code/mainPage.js
@@ -1,6 +1,7 @@
 /**
  * Go to task list page
  */
+
 function viewTaskList() {
   location.href = 'taskList.html';
 }
@@ -15,4 +16,19 @@ function viewSettings() {
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('viewTasksBtn').onclick = viewTaskList;
   document.getElementById('setting_img').onclick = viewSettings;
+  document.getElementById('submitTaskBtn').onclick = submitTask;
+
 });
+
+function submitTask() {
+	var name = document.getElementById('inputTask').value;
+	var session = document.getElementById('sessionId').value;
+	var group = document.getElementById('groups').value;
+	var result2 = addGroup(group);
+	console.log(result2);
+	console.log(group);
+	var result =  addTask(name, session, group);
+	console.log(result);
+
+	
+}
